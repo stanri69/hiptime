3e3123a378e78d044463bb489a54f92c6cd323f4
stas <stanri@debian-BULLSEYE-live-builder-AMD64>
2021-11-15 02:33:39 +0200

    Add conditional statement to index action and view file

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,9 @@ class ItemsController < ApplicationController
   before_action :find_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @item = Item.all.order("created_at DESC")
+    if user_signed_in?
+      @item = Item.where(:user_id => current_user.id).order("created_at DESC")
+    end
   end
 
   def show
